799b062e722814c625ebae411206995a8131f769
Jovon Craig <jovoncraig1@gmail.com>
2020-05-13 15:21:00 -0400

    Handle filters property in Lambda function

--- src/utils/lambda_function.py
+++ src/utils/lambda_function.py
@@ -3,19 +3,52 @@ import boto3
 
 def lambda_handler(event, context):
     body = json.loads(event['body'])
+    client = boto3.client('dynamodb')
+
+    if event['rawPath'] == '/attribute':
+        attribute_names = {'#N': 'Name', '#A': body['attribute']}
+        attribute_values = {}
+        filter_expression = ''
+
+        for i, filter in enumerate(body['filters']):
+            if i != 0:
+                filter_expression += ' and '
+            tokens = filter.split()
+            op_idx = next(i for i, token in enumerate(tokens)
+                if token in ('=', '<', '<=', '>', '>='))
+            attribute_names['#' + str(i)] = ' '.join(tokens[:op_idx])
+            endTokens = tokens[op_idx+1:]
+
+            firstNum = (endTokens[0][1:] if endTokens[0][0] == '$'
+                else endTokens[0]).replace(',', '')
+            attribute_values[':' + str(i)] = {'N': firstNum}
+            if len(endTokens) == 1:
+                filter_expression += '#{0} {1} :{0}'.format(str(i), tokens[op_idx])
+            else:
+                secondNum = (endTokens[2][1:] if endTokens[2][0] == '$'
+                    else endTokens[2]).replace(',', '')
+                attribute_values[':' + str(i) + 'a'] = {'N': secondNum}
+                filter_expression += "#{0} between :{0} and :{0}a".format(str(i))
+
+        kwargs = {
+            'TableName': body['resolution'],
+            'ExpressionAttributeNames': attribute_names,
+            'ProjectionExpression': 'ID, #N, #A'
+        }
+        if filter_expression:
+            kwargs.update({
+                'ExpressionAttributeValues': attribute_values,
+                'FilterExpression': filter_expression
+            })
+        result = client.scan(**kwargs)
+    else:
+        result = client.query(
+            TableName=body['resolution'],
+            ExpressionAttributeValues={':featureId': {'S': body['featureId']}},
+            KeyConditionExpression='ID = :featureId'
+        )
 
     return {
         'statusCode': 200,
-        'body': json.dumps(
-            boto3.client('dynamodb').scan(
-                TableName=body['resolution'],
-                ExpressionAttributeNames={'#N': 'Name', '#A': body['attribute']},
-                ProjectionExpression='ID, #N, #A'
-            ) if event['rawPath'] == '/attribute' else
-            boto3.client('dynamodb').query(
-                TableName=body['resolution'],
-                ExpressionAttributeValues={':featureId': {'S': body['featureId']}},
-                KeyConditionExpression='ID=:featureId'
-            )
-        )
+        'body': json.dumps(result)
     }
