79328d1dbee1689da486edf443f1db906d9ebfa3
Jovon Craig <jovoncraig1@gmail.com>
2020-05-27 13:21:18 -0400

    Reduce size of shapefiles

--- src/utils/create_shapefiles.py
+++ src/utils/create_shapefiles.py
@@ -18,15 +18,29 @@ for res_name, res_abbr in resolutions.items():
         shx=file.open('tl_2018_us_' + file_res_abbr + '.shx')
     )
 
-    # Adapted from Frank Rowe (https://gist.github.com/frankrowe/6071443)
-    features = [{
-        "type": "Feature",
-        "geometry": shape_record.shape.__geo_interface__,
-        "properties": dict(zip([f[0] for f in sf.fields[1:]],
-                               shape_record.record))
-    } for shape_record in sf.shapeRecords()]
+    features = []
+    for shape_record in sf.shapeRecords():
+        geometry = shape_record.shape.__geo_interface__
+        if geometry['type'] == 'Polygon':
+            geometry['coordinates'] = [[
+                coord for i, coord in enumerate(geometry['coordinates'][0])
+                if i % 20 == 0]]
+        else:
+            for i, coords in enumerate(geometry['coordinates']):
+                geometry['coordinates'][i] = [[
+                    coord for i, coord in enumerate(coords[0])
+                    if i % 20 == 0]]
+
+        # Adapted from Frank Rowe (https://gist.github.com/frankrowe/6071443)
+        features.append({
+            "type": "Feature",
+            "geometry": geometry,
+            "properties": dict(zip([f[0] for f in sf.fields[1:]],
+                                   shape_record.record))
+        })
 
     folder_path = '../assets/shapefiles/'
     os.makedirs(folder_path, exist_ok=True)
     open(folder_path + res_name + '.json', 'w').write(json.dumps(
-        {"type": "FeatureCollection", "features": features}))
+        {"type": "FeatureCollection", "features": features})
+            .replace(' ', '').replace('9' * 8, '').replace('0' * 9, ''))
